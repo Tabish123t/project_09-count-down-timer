383d45135d20728ea71f674358f5d411bc76c2fa
Tabish123t <tabishyunus10@gmail.com>
2024-09-08 01:13:36 +0500

    project_09 count down timer

---
+++ index.js
@@ -0,0 +1,43 @@
+import inquirer from 'inquirer';
+class CountdownTimer {
+    duration;
+    intervalId = null;
+    constructor(duration) {
+        this.duration = duration;
+    }
+    start() {
+        this.intervalId = setInterval(() => {
+            if (this.duration > 0) {
+                this.duration--;
+                console.log(`Time left: ${this.duration} seconds`);
+            }
+            else {
+                console.log('Time is up!');
+                this.stop();
+            }
+        }, 1000);
+    }
+    stop() {
+        if (this.intervalId) {
+            clearInterval(this.intervalId);
+            this.intervalId = null;
+        }
+    }
+}
+async function getUserInput() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'duration',
+            message: 'Enter the countdown duration in seconds:',
+            validate: (input) => {
+                const num = parseInt(input, 10);
+                return !isNaN(num) && num > 0 ? true : 'Please enter a valid number greater than 0';
+            }
+        }
+    ]);
+    const duration = parseInt(answers.duration, 10);
+    const timer = new CountdownTimer(duration);
+    timer.start();
+}
+getUserInput();
